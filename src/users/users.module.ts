9d89499c9968a16771833ae92127a0d0a932c642
dsad233 <ksl1421@naver.com>
2026-01-08 23:43:32 +0900

    fix: users module 수정

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -4,12 +4,29 @@ import { UsersController } from './controllers/users.controller';
 import { CqrsModule } from '@nestjs/cqrs';
 import { UsersRepository } from './users.repoository';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersHandler } from './handlers/users.handler';
+import { UserCreateHandler } from './handlers/users.create.handler';
 import { User, UserDetail } from 'libs/entities';
+import { UserCreateEventHandler } from './handlers/users.create.event.handler';
+import { UserCreateQueryHandler } from './handlers/users.create.query.handler';
+import { MailerModule } from 'src/mailer/mailer.module';
+
+const CommandHandler = [UserCreateHandler];
+const EventHandler = [UserCreateEventHandler];
+const QueryHandler = [UserCreateQueryHandler];
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserDetail]), CqrsModule],
+  imports: [
+    TypeOrmModule.forFeature([User, UserDetail]),
+    CqrsModule,
+    MailerModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, UsersHandler],
+  providers: [
+    UsersService,
+    UsersRepository,
+    ...CommandHandler,
+    ...EventHandler,
+    ...QueryHandler,
+  ],
 })
 export class UsersModule {}
