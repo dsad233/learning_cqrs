4ac70590dcd743f90bca1b4e137cb9d5bb51eff6
dsad233 <ksl1421@naver.com>
2026-01-27 23:42:17 +0900

    feat: authguard 추가

---
+++ src/common/certification/auth-guard.ts
@@ -0,0 +1,41 @@
+import { TYPE } from '@libs/enums';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { JwtService } from 'src/jwt/jwt.service';
+
+const authToken = 'user';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException(
+        '토큰이 존재하지 않습니다. 다시 로그인 해주세요.',
+      );
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        TYPE.TokenTypeEnum.ACCESS,
+      );
+      request[authToken] = payload;
+    } catch {
+      throw new UnauthorizedException('올바르지 않은 유저 정보 입니다.');
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
