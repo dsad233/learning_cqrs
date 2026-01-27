97582e7051754eb62542bb8d204cf95f22e076f1
dsad233 <ksl1421@naver.com>
2026-01-27 23:31:43 +0900

    fix: eslint 개행 오류 완화 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,12 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    // 맥OS 환경의 개행 방식과 Windows의 환경에서의 개행 방식이 달라, 해당 설정이 필요
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
   },
 };
