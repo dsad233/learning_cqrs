2414255625c0f34b98d7938feb625b54192db63d
dsad233 <ksl1421@naver.com>
2026-01-30 22:41:26 +0900

    feat: 전역가드 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,10 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseConfig } from './common/configs/database.config';
 import configuration from './common/configs/configuration';
 import { validationSchema } from './common/configs/validationSchema';
-import { APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { LoggingInterceptor } from './common/interceptor/logging-Interceptor';
+import { AuthGuard } from './common/certification/auth.guard';
+import { JwtModule } from './jwt/jwt.module';
 
 @Module({
   imports: [
@@ -23,10 +25,15 @@ import { LoggingInterceptor } from './common/interceptor/logging-Interceptor';
       useClass: DatabaseConfig,
     }),
     UsersModule,
+    JwtModule,
   ],
   controllers: [AppController],
   providers: [
     AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
     {
       provide: APP_INTERCEPTOR,
       useClass: LoggingInterceptor,
